d39c3742e9812807dffc99a757b59be739a4bda7
s18011 <s18011@std.it-college.ac.jp>
2019-01-16 06:55:52 +0000

    1220

---
+++ 1220/1220.3.sql
@@ -0,0 +1,6 @@
+select
+   round(max(salary)) "Maximum",
+   round(min(salary)) "Minimum",
+   round(sum(salary)) "Sum",
+   round(avg(salary)) "Average"
+from employees;
---
+++ 1220/1220.5.sql
@@ -0,0 +1,4 @@
+select job_id, count(job_id)
+from employees
+group by job_id
+order by job_id;
