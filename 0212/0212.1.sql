af35273918f085b52728ea8317a8a8cb922a6d86
s18011 <s18011@std.it-college.ac.jp>
2019-02-26 05:00:44 +0000

    kadai

---
+++ 0212/0212.1.sql
@@ -0,0 +1,8 @@
+create table member(
+    member_id number(10),
+    last_name varchar(25),
+    first_name varchar2(25),
+    address varchar2(100),
+    city varchar2(30),
+    phone varchar2(15),
+    join_date date);
---
+++ 0212/0212.2.sql
@@ -0,0 +1,4 @@
+alter table member
+    add constraint membar_member_id_pk
+    primary key(member_id);
+
